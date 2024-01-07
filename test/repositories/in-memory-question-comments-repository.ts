c4c095097e4f67fc7fc09e7707b0c6840304a16a
Pedro Requiao <alonsofts@gmail.com>
2024-01-06 22:40:16 -0500

    add: delete comment on question use case and tests

--- test/repositories/in-memory-question-comments-repository.ts
+++ test/repositories/in-memory-question-comments-repository.ts
@@ -1,4 +1,3 @@
-
 import { QuestionCommentsRepository } from '@/domain/forum/application/repositories/question-commments-repository'
 import { QuestionComment } from '@/domain/forum/enterprise/entities/question-comment'
 
@@ -6,7 +5,25 @@ export class InMemoryQuestionCommentsRepository
     implements QuestionCommentsRepository {
     public items: QuestionComment[] = []
 
+    async findById(id: string) {
+        const questionComment = this.items.find((item) => item.id.toString() === id)
+
+        if (!questionComment) {
+            return null
+        }
+
+        return questionComment
+    }
+
     async create(questionComment: QuestionComment) {
         this.items.push(questionComment)
+    }
+
+    async delete(questionComment: QuestionComment) {
+        const itemIndex = this.items.findIndex(
+            (item) => item.id === questionComment.id,
+        )
+
+        this.items.splice(itemIndex, 1)
     }
 }
