2a708bdb3458f20b7721e13b72e1513bfe76f886
Pedro Requiao <alonsofts@gmail.com>
2024-01-06 21:25:35 -0500

    add: Fetch question answer use case and test

--- src/domain/forum/application/repositories/answers-repository.ts
+++ src/domain/forum/application/repositories/answers-repository.ts
@@ -1,7 +1,9 @@
+import { PaginationParams } from '@/core/repositories/pagination-params'
 import { Answer } from '../../enterprise/entities/answer'
 
 export interface AnswersRepository {
     findById(id: string): Promise<Answer | null>
+    findManyByQuestionId(questionId: string, params: PaginationParams): Promise<Answer[]>
     save(question: Answer): Promise<void>
     create(answer: Answer): Promise<void>
     delete(answer: Answer): Promise<void>
--- test/repositories/in-memory-answers-repository.ts
+++ test/repositories/in-memory-answers-repository.ts
@@ -1,3 +1,4 @@
+import { PaginationParams } from "@/core/repositories/pagination-params"
 import { AnswersRepository } from "@/domain/forum/application/repositories/answers-repository"
 import { Answer } from "@/domain/forum/enterprise/entities/answer"
 
@@ -5,6 +6,14 @@ import { Answer } from "@/domain/forum/enterprise/entities/answer"
 export class InMemoryAnswersRepository implements AnswersRepository {
 
     public items: Answer[] = []
+
+    async findManyByQuestionId(questionId: string, { page }: PaginationParams) {
+        const awnswers = this.items
+            .filter(item => item.questionId.toString() === questionId)
+            .slice((page - 1) * 20, page * 20)
+
+        return awnswers
+    }
 
     async findById(id: string) {
         const answer = this.items.find(item => item.id.toString() === id)
