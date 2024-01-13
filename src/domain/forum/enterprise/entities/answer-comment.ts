138c9b8787eaa3e11b0a396dc73656c8b942ea0b
Pedro Requiao <alonsofts@gmail.com>
2024-01-13 04:44:17 -0500

    add: comment on answer event

--- src/domain/forum/enterprise/entities/answer-comment.ts
+++ src/domain/forum/enterprise/entities/answer-comment.ts
@@ -1,6 +1,7 @@
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import { Optional } from '@/core/types/optional'
 import { Comment, CommentProps } from './comment'
+import { AnswerCommentCreatedEvent } from '../events/answer-comment-created-event'
 
 export interface AnswerCommentProps extends CommentProps {
     answerId: UniqueEntityID
@@ -22,6 +23,12 @@ export class AnswerComment extends Comment<AnswerCommentProps> {
             },
             id,
         )
+
+        const isNewAnswerComment = !id
+
+        if (isNewAnswerComment) {
+            answerComment.addDomainEvent(new AnswerCommentCreatedEvent(answerComment))
+        }
 
         return answerComment
     }
